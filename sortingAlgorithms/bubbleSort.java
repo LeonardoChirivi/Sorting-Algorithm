d68c767c999ad557246184e2bce6cf02048fa2b5
Leonardo Chirivì <leonardochirivi1@gmail.com>
2015-12-14 13:49:42 +0100

    Sorting algorithm

--- sortingAlgorithms/bubbleSort.java
+++ sortingAlgorithms/bubbleSort.java
@@ -4,32 +4,38 @@ import java.util.Arrays;
 
 public class bubbleSort {
 
-	public static void main(String[] args) {
-		
-		int[] array = new int[(int) (Math.random() *20)];
-		
-		for( int i = 0; i < array.length; i++ )
-			array[i] = (int) (Math.random() * 20);
-		
-		System.out.println(Arrays.toString(array)+"\n");
-		
+	public static void sort(int[] array) {
+
 		for( int i = array.length-1; i > 1; i-- ){		
 			for( int j = 0; j < i; j++ ){
-				
+
 				if( array[j] > array[j+1] ){
-					
+
 					array[j] = array[j] + array[j+1];
 					array[j+1] = array[j] - array[j+1];
 					array[j] = array[j] - array[j+1];
-					
+
 				}
-				
+
 			}
-			
+
 		}
-		
-		System.out.println(Arrays.toString(array));
-	
+
 	}
-	
+
+	public static void main(String[] args) {
+
+		int[] array = new int[(int) (Math.random() *20)];
+
+		for( int i = 0; i < array.length; i++ )
+			array[i] = (int) (Math.random() * 20);
+
+		System.out.println(Arrays.toString(array)+"\n");
+
+		sort(array);
+
+		System.out.println(Arrays.toString(array));
+
+	}
+
 }
